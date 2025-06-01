f8743e7c8ab538156f61ad87056ddaedb3023ddb
Svemoćan <sarajevskimudar@gmail.com>
2025-06-01 13:24:33 +0200

    bot
    now includes: bot

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.environ['TOKEN']
+ROLE_ID = int(os.environ['ROLE_ID'])
+CHANNEL_ID = int(os.environ.get('CHANNEL_ID', 0))  # default 0 = no specific channel
+SPECIFIC_USER_ID = int(os.environ.get('SPECIFIC_USER_ID', 0))  # default 0 = no specific user
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.messages = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    print(f'{bot.user} just sent a message.')
+    #if message.author.bot:
+    #    return
+
+    if CHANNEL_ID and message.channel.id != CHANNEL_ID:
+        return
+
+    if SPECIFIC_USER_ID and message.author.id != SPECIFIC_USER_ID:
+        return
+
+    role = message.guild.get_role(ROLE_ID)
+    if role:
+        await message.channel.send(f'Aha {role.mention}, Wargaming vydaly nějakou novinku, připravte si kreditku! :grin:')
+
+bot.run(TOKEN)
