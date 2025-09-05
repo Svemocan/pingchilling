58b0bfd1280a8e15fab9abc70cd050d920ca2e33
Svemoćan <sarajevskimudar@gmail.com>
2025-09-05 21:50:40 +0200

    test2
    test2

--- main.py
+++ main.py
@@ -22,31 +22,32 @@ choices = [
     # Generické
     "Aha {role.mention}, ve Wargamingu zase něco vymyslely...běžte si to přečíst, než si to zmizí. :eyes:",
     "Hey ho {role.mention}, čerstvé zprávy z WG!",
-    "{role.mention}, máme tu nové info – jestli dobré, nebo zabugované, to už musíte zjistit sami. :kek:",
+    "{role.mention}, máme tu nové info – jestli dobré, nebo zabugované, to už musíte zjistit sami. <:kek:1231645829148971098>",
     "Nové info {role.mention}, co to tak může být?",
-    "{role.mention}, dorazil další update. Možná i funguje! :kekleo:",
+    "{role.mention}, dorazil další update. Možná i funguje! <:kekleo:1243212747181916282>",
     "Zprávy přímo z Wargamingu {role.mention}.",
-    "{role.mention}, pozor! WG něco vypustilo do světa. Snad to nesmrdí. :pain:",
-    "WG vydalo čerstvou dávku textu. {role.mention}, přečíst, nepřešíst, toť otázka. :pepeshrug:",
+    "{role.mention}, pozor! WG něco vypustilo do světa. Snad to nesmrdí. <:pain:1243213799264227431>",
+    "WG vydalo čerstvou dávku textu. {role.mention}, přečíst, nepřešíst, toť otázka. <:pepeshrug:1367089196749160448>",
     "{role.mention}, další díl seriálu \"Co je nového ve WoT\". :tv:",
-    "Hlášení z fronty: {role.mention}, na obzoru něco nového! Snad pozitivního... :doge:",
+    "Hlášení z fronty: {role.mention}, na obzoru něco nového! Snad pozitivního... <:doge:1243211891116085319>",
     "{role.mention}, čerstvé zprávy – nebo jen další changelog. Ja neznaju, ja jenom dumb bot :zany_face:",
     "Hlásim pohyb na radaru: WG news. {role.mention}, připravit rozkladací židle a dalekohledy! :satellite:",
     "{role.mention}, je to tu zas. Novinky, patch, update… hoďte si kostkou. :game_die:",
-    "WG zmáčklo tlačítko ‚Publish‘. {role.mention}, tak se na to podívejme. :pepesearch:"
+    "WG zmáčklo tlačítko ‚Publish‘. {role.mention}, tak se na to podívejme. <:pepesearch:1367089084463583354>"
     "{role.mention}, update detekován! Beep Boop :robot:",
 
     # Satirické
-    "{role.mention}, WG vydalo něco nového. Buď bugfix, nebo další důvod k smíchu :kekw:",
-    "Pozor {role.mention}, další patch. Tipuju, že zase rozbije něco jiného :lulw:",
-    "{role.mention}, nové novinky. Kurzy: 20% event, 30% bugfix, 50% monetizace. :BjarneKekLeft:",
+    "{role.mention}, WG vydalo něco nového. Buď bugfix, nebo další důvod k smíchu <:kekw:1231355571400085634>",
+    "Pozor {role.mention}, další patch. Tipuju, že zase rozbije něco jiného <:lulw:1243213559152775249>",
+    "{role.mention}, nové novinky. Kurzy: 20% event, 30% bugfix, 50% monetizace. <:BjarneKekLeft:1231646307429388318>",
     "WG zase něco přidalo. {role.mention}, asi další \"feature\". :smirk:",
-    "{role.mention}, čerstvé zprávy – a možná i čerstvá dávka nas*ání. :pepeno:",
-    "Breaking news: {role.mention}, WG našlo další důvod na restart klienta. :pog:",
-    "{role.mention}, další novinka. Prosíme o potlesk, hoši ve WG makaj o sto šest! :pepewicked:",
-    "{role.mention}, Wargaming vydaly něco zbrusu nového, again. Ako bonus: frustrace zdarma. :pepecool:",
+    "{role.mention}, čerstvé zprávy – a možná i čerstvá dávka nas*ání. <:pepeno:1367087667363315812>",
+    "Breaking news: {role.mention}, WG našlo další důvod na restart klienta. <:pog:1243208206696845504>",
+    "{role.mention}, další novinka. Prosíme o potlesk, hoši ve WG makaj o sto šest! <:pepewicked:1231645860077633567>",
+    "{role.mention}, Wargaming vydaly něco zbrusu nového, again. Ako bonus: frustrace zdarma. <:pepecool:1367087798787637318>",
     "Aha {role.mention}, Wargaming vydaly nějakou novinku, připravte si kreditku! :grin:"
 ]
+
 
 @bot.event
 async def on_ready():
@@ -55,14 +56,15 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    role = message.guild.get_role(ROLE_ID)
 
     user = message.author
     #if user.bot:
     #    return
 
-    if TEST_ID and message.channel.id == CHANNEL_ID and user.id == TEST_ID:
-        if message.content.startswith("~test"):
-            await message.channel.send(f'{user.mention} test :boykisser:')
+    if TEST_ID and user.id == TEST_ID:
+        if message.content.startswith("~pingchillingtest"):
+            await message.channel.send(f'{user.mention} test <:boykisser:1243213206562799657>')
             message = random.choices(choices)
             await message.channel.send(f"{message}")
             print('Test :3')
@@ -75,8 +77,8 @@ async def on_message(message):
 
     role = message.guild.get_role(ROLE_ID)
     if role:
-        message = random.choices(choices)
-        await message.channel.send(f"{message}")
+        chosen = random.choice(choices).format(role=role)
+        await message.channel.send(chosen)
         print(f'{bot.user} just sent a message.')
 
 
